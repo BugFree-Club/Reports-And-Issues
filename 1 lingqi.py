fab96d5566f5007d7edc73c39b144510e64b62a8
lqfickle <46953689+ficklelq@users.noreply.github.com>
2019-01-25 01:10:11 -0600

    ddd
    凌齐

---
+++ 1 lingqi.py
@@ -0,0 +1,15 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a+b;
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
+SyntaxError: invalid syntax
+>>> 
+>>> 
