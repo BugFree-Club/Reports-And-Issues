3d2f0a8d32195288dc71710f34881b8a33918dcc
assassin945 <40627941+assassin945@users.noreply.github.com>
2019-03-07 15:38:20 +0800

    Add files via upload

---
+++ question-2019-3-7/DataStructures-Chain.cpp
@@ -0,0 +1,106 @@
+﻿// DataStructures-Chain.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include "chain.h"
+#include "linearList.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// test constructor
+	linearList<double> *x = new chain<double>;
+	chain<int> y, z;
+
+	// test size
+	cout << "Initial size of x, y, and z = "
+		<< x->size() << ", "
+		<< y.size() << ", "
+		<< z.size() << endl;
+
+	// test empty
+	if (x->empty()) cout << "x is empty" << endl;
+	else cout << "x is not empty" << endl;
+	if (y.empty()) cout << "y is empty" << endl;
+	else cout << "y is not empty" << endl;
+
+	// test insert
+	y.insert(0, 2);
+	y.insert(1, 6);
+	y.insert(0, 1);
+	y.insert(2, 4);
+	y.insert(3, 5);
+	y.insert(2, 3);
+	cout << "Inserted 6 integers, list y should be 1 2 3 4 5 6" << endl;
+	cout << "Size of y = " << y.size() << endl;
+	if (y.empty()) cout << "y is empty" << endl;
+	else cout << "y is not empty" << endl;
+	y.output(cout);
+	cout << endl << "Testing overloaded <<" << endl;
+	cout << y << endl;
+
+	// test indexOf
+	int index = y.indexOf(4);
+	if (index < 0) cout << "4 not found" << endl;
+	else cout << "The index of 4 is " << index << endl;
+
+	index = y.indexOf(7);
+	if (index < 0) cout << "7 not found" << endl;
+	else cout << "The index of 7 is " << index << endl;
+
+	// test get
+	cout << "Element with index 0 is " << y.get(0) << endl;
+	cout << "Element with index 3 is " << y.get(3) << endl;
+
+	// test erase
+	y.erase(1);
+	cout << "Element 1 erased" << endl;
+	cout << "The list is " << y << endl;
+	y.erase(2);
+	cout << "Element 2 erased" << endl;
+	cout << "The list is " << y << endl;
+	y.erase(0);
+	cout << "Element 0 erased" << endl;
+	cout << "The list is " << y << endl;
+
+	cout << "Size of y = " << y.size() << endl;
+	if (y.empty()) cout << "y is empty" << endl;
+	else cout << "y is not empty" << endl;
+
+	try { y.insert(-3, 0); }
+	catch (illegalIndex e)
+	{
+		cout << "Illegal index exception" << endl;
+		cout << "Insert index must be between 0 and list size" << endl;
+		e.outputMessage();
+	}
+
+	// test copy constructor
+	chain<int> w(y);
+	y.erase(0);
+	y.erase(0);
+	cout << "w should be old y, new y has first 2 elements removed" << endl;
+	cout << "w is " << w << endl;
+	cout << "y is " << y << endl;
+
+	// a few more inserts, just for fun
+	y.insert(0, 4);
+	y.insert(0, 5);
+	y.insert(0, 6);
+	y.insert(0, 7);
+	cout << "y is " << y << endl;
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
